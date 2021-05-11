c077a1660dbfe8fa820f6d77a8e9dbd79cdb1836
2020software <softwaregem2020@gmail.com>
2021-05-12 00:06:57 +0900

    https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app" class="home-screen">
@@ -78,6 +78,6 @@
             'user' => Auth::user()  // ログイン中のユーザー情報
         ]) !!}
     </script>
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
