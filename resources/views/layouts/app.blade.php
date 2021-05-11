585a064daffd7577930f9045ae9d5f11401dc923
2020software <softwaregem2020@gmail.com>
2021-05-12 00:45:16 +0900

    https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,6 @@
             'user' => Auth::user()  // ログイン中のユーザー情報
         ]) !!}
     </script>
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
