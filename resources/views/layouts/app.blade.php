dde632f98ced3642294d9e7a2379883728c2455d
2020software <softwaregem2020@gmail.com>
2021-05-12 00:17:08 +0900

    https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ secure_config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -78,6 +78,6 @@
             'user' => Auth::user()  // ログイン中のユーザー情報
         ]) !!}
     </script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 </body>
 </html>
